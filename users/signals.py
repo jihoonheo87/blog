3b581d4623b618ec2cae0f406db23fe50935adb7
Jihoon Heo <Jihoon.heo@gmail.com>
2020-01-07 06:33:33 -0800

    update feature, put profile picutre on blog

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
@@ -10,4 +11,4 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.Profile.save()
+    instance.profile.save()    
